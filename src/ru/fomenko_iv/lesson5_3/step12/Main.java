be36dc165dea907e36e95438d7dd2415f005a2cb
FomeIV <harryfom@gmail.com>
2016-03-24 15:20:27 +0300

    5.3 Потоки символов – Шаг 12 Begin

---
+++ src/ru/fomenko_iv/lesson5_3/step12/Main.java
@@ -0,0 +1,17 @@
+package ru.fomenko_iv.lesson5_3.step12;
+
+/*
+Реализуйте метод, который зачитает данные из InputStream и преобразует их в строку, используя заданную кодировку.
+
+Пример
+InputStream последовательно возвращает четыре байта: 48 49 50 51.
+Метод, вызванный для такого InputStream и кодировки ASCII, должен вернуть строку "0123".
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("5.3 Потоки символов – Шаг 12");
+    }
+
+
+}
